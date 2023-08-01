0f1fe3cfbd6e604a732c27e7c3d9e05b0f241566
Eugênio Rodrigues <eugenio28.04@gmail.com>
2023-08-01 17:29:36 -0300

    adição das outras telas e padronização do header para todas

---
+++ SCRIPTS/scriptBtnSair.js
@@ -0,0 +1,11 @@
+const btnSair = document.querySelector('[data-btn-sair]');
+
+btnSair.addEventListener('click', () => {
+    const confirmacaoSaida = confirm('Deseja mesmo sair do sistema?');
+
+    console.log(confirmacaoSaida);
+
+    if (confirmacaoSaida) {
+        window.location.replace('./index.html');
+    }
+});
